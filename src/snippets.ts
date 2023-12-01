e08b2c0eb57fd13c6b5437bfca9bc5f37094c27a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-12-01 11:19:22 +0100

    Legg til en snippet til

--- src/snippets.ts
+++ src/snippets.ts
@@ -9,8 +9,8 @@ const kortkomponentFlex = {
     <p>En lengre beskrivelse</p>
   </div>
 </div>
-`,
-    css: code`
+
+<style>
 .kort {
   display: flex;
   gap: 1rem;
@@ -21,9 +21,59 @@ const kortkomponentFlex = {
   flex-direction: column;
   gap: 0.5rem;
 }
+</style>
+`,
+    css: code`
+/* Dette er bare litt styling for å gjøre
+   eksempelet penere. */
+body {
+  margin: 1rem;
+}
 
+.kort {
+  border: 2px solid black;
+  border-radius: 4px;
+  width: 250px;
+  padding: 1rem;
+}
 
-/* Uinteressant */
+.ikon {
+  border-radius: 50%;
+  background-color: black;
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+h2, p {
+  margin: 0;
+}
+`,
+};
+
+const kortkomponentGrid = {
+    html: code`
+<div class="kort">
+  <div class="ikon"></div>
+  <h2>En kort tittel</h2>
+  <p class="beskrivelse">En lengre beskrivelse</p>
+</div>
+
+<style>
+.kort {
+  display: grid;
+  grid-template-columns: 1.5rem 1fr;
+  align-items: center;
+  gap: 0.5rem 1rem;
+}
+
+.beskrivelse {
+  grid-column: 2;
+}
+</style>
+`,
+    css: code`
+/* Dette er bare litt styling for å gjøre
+   eksempelet penere. */
 body {
   margin: 1rem;
 }
@@ -50,4 +100,5 @@ h2, p {
 
 export default {
     'kort-flex': kortkomponentFlex,
+    'kort-grid': kortkomponentGrid,
 };
